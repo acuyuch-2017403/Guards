6ed1e1f0e9a0d0f4be59e201dd1035e49829b069
acuyuch-2017403 <acuyuch-2017403@kinal.edu.gt>
2022-05-22 17:22:31 -0600

    Guards

---
+++ src/app/components/products/products.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductRestService } from 'src/app/services/productRest/product-rest.service';
+import { CategoryRestService } from 'src/app/services/categoryRest/category-rest.service';
+import { ProductModel } from 'src/app/models/product.model';
+import { UserRestService } from 'src/app/services/userRest/user-rest.service';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+  products:any;
+  categorys:any;
+  product: ProductModel;
+
+  constructor(
+    private productRest: ProductRestService,
+    private categoryRest: CategoryRestService,
+    private userRest: UserRestService
+    
+  ) { 
+    this.product = new ProductModel('','','',0,0,0,'');
+  }
+
+  ngOnInit(): void {
+    this.getProducts();
+    this.getCategorys();
+  }
+
+  getProducts(){
+    this.productRest.getProducts().subscribe({
+      next: (res:any)=> this.products = res.products,
+      error: (err)=>alert(err.error.message)
+    })
+  }
+
+  getCategorys(){
+    this.categoryRest.getCategorys().subscribe({
+      next: (res:any)=> this.categorys = res.categorys,
+      error: (err)=>console.log(err)
+    })
+  }
+
+  saveProduct(){
+    this.productRest.saveProduct(this.product).subscribe({
+      next: (res:any)=> alert(res.message),
+      error: (err)=> alert(err.error.message || err.error)
+    })
+  }
+
+}
